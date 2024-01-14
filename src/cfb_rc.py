b608bdfee4ef74dce0067b782244e7f294b96020
usbin <winterer0222@gmail.com>
2024-01-14 14:15:01 +0900

    .

--- src/cfb_rc.py
+++ src/cfb_rc.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:439c4fd1216b5a5323ec6057b7fe47eab6b89f678d783f00d9fd045a124a21a6
-size 144139778
+oid sha256:54be4d41e515713ce3abe531404a628dce160437f73a6df507eeff17a0b224cd
+size 106261827
